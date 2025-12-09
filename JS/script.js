656486cf3bffb47a8697816e2920d2bac9b1d0a7
Stanislav <example@gmail.com>
2025-12-09 10:48:03 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,43 @@
+"use strict";
+// a = 5;
+// console.log(number2);
+let number = 5;
+const leftBorderWidth = 1;
+var number2 = 50;
+
+number = 10;
+console.log(number);
+
+// leftBorderWidth = 2;
+// console.log(leftBorderWidth);
+
+const obj = {
+  a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+number = 4.6;
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = 'Alex';
+
+let und;
+console.log(und);
+
+const obj = {
+  name: "John",
+  age: 25,
+  isMarried: false
+}
+
+console.log(obj.age);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+
+console.log(arr[1]);
+
+const arrr = [1, 2, 3];
